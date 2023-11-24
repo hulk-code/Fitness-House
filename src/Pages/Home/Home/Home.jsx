ae9e8e9e2fdc2332ab7b0aa35b8c7f00d76bd275
Akash <shoaibsarkarakash019@gmail.com>
2023-11-25 03:08:14 +0600

    blog page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import AboutUs from "../../AboutUs/AboutUs";
 import Banner from "../../Banner/Banner";
 import Classes from "../../Classes/Classes";
 import ClientReview from "../../ClientReview/ClientReview";
+import Posts from "../../Posts/Posts";
 
 import FeaturedSection from "../FeaturedSection/FeaturedSection";
 
@@ -16,6 +17,7 @@ const Home = () => {
             <AboutUs></AboutUs>
             <Classes></Classes>
           <ClientReview></ClientReview>
+          <Posts></Posts>
 
         </div>
     );
