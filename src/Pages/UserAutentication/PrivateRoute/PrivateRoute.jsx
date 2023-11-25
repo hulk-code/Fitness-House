f0df5d03bef45a88127ddd3adbf6e3be4d572de5
Akash <shoaibsarkarakash019@gmail.com>
2023-11-26 00:31:05 +0600

    private route

---
+++ src/Pages/UserAutentication/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,30 @@
+
+
+
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../../../Hook/UseAuth/UseAuth";
+
+
+const PrivateRoute = ({children}) => {
+     const {user ,loading}=useAuth()
+     const location=useLocation();
+     console.log(loading);
+     
+     if(loading){
+        return <progress className="progress w-56"></progress>
+     }
+     console.log(user);
+     if(user){
+        return children ;
+        
+    }
+
+      
+
+
+    
+    
+    return <Navigate to='/login' state={{form: location}} replace></Navigate>
+};
+
+export default PrivateRoute;
